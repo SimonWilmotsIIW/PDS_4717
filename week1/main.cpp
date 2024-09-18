c9b2b1ce73d046446c4557abc047a5af611ad53b
SimonWilmotsIIW <simon.wilmots@student.uhasselt.be>
2024-09-18 17:22:28 +0200

    find min/max

--- week1/main.cpp
+++ week1/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <stdio.h>
+#include "readfloats.cpp"
+#include <limits>
+
+std::pair<float, float> findMinMax(std::vector<float> data) {
+    float max = std::numeric_limits<float>::min();
+    float min = std::numeric_limits<float>::max();
+
+    for (int i=0 ; i < data.size() ; i++){
+        if (data[i] > max) {
+            max = data[i];
+        }
+
+        if (data[i] < min) {
+            min = data[i];
+        }
+    }
+    return std::pair<float, float>(min,max);
+}
 
 int main() {
-    std::cout << "Hello World!";
+    std::vector<float> data = readFloats("histvalues.dat");
+    // for(int i=0 ; i<data.size() ; i++) {
+    //     printf("%f\n", data[i]);
+    // }
+
+    std::pair<float, float> minMax = findMinMax(data);
+
+    printf("\nMin: %f Max: %f", minMax.first, minMax.second);
+
     return 0;
 }
